9df12d45acc78a6292cd9ae809dace06f9d8b371
jaime <jaimepereirasantos123@gmail.com>
2021-12-18 01:03:52 +0000

    Trying to clean up time measurement, but something is wrong

--- test-main.py
+++ test-main.py
@@ -11,96 +11,85 @@ from QuantumWalkTest.QuantumWalkDaoTest import QuantumWalkDaoTest
 
 
 def getAvgTime(timeList):
-    totalTime = 0
-    for t in timeList:
-        totalTime += t
-    return totalTime/len(timeList)
+    te = 0
+    te2 = 0
+    te3 = 0
+    tm = 0
+    tm2 = 0
+    tm3 = 0
+    tf = 0
+    tf2 = 0
+    tf3 = 0
+    for i in range(0, len(timeList), 9):
+        if(i + 9 > len(timeList)):
+            break
+        te += timeList[i]
+        te2 += timeList[i+1]
+        te3 += timeList[i+2]
+        tm += timeList[i+3]
+        tm2 += timeList[i+4]
+        tm3 += timeList[i+5]
+        tf += timeList[i+6]
+        tf2 += timeList[i+7]
+        tf3 += timeList[i+8]
+    return [te/len(timeList), te2/len(timeList), te3/len(timeList), 
+            tm/len(timeList), tm2/len(timeList), tm3/len(timeList), 
+            tf/len(timeList), tf2/len(timeList), tf3/len(timeList)]
+
+
+def createTimeList(samples, n, graph, t, gamma, marked):
+    timeList = []
+    for i in range(samples):
+        print("\n--------- Trial #%s -------------------\n" % i)
+        qwCont = QuantumWalkDaoTest(n, graph, t, gamma, marked)
+        timeList.extend([qwCont.eighExecutionTime, qwCont.eighExecutionTime2, qwCont.eighExecutionTime3,
+                        qwCont.matMulExecutionTime, qwCont.matMulExecutionTime2, qwCont.matMulExecutionTime3,
+                        qwCont.fullExecutionTime, qwCont.fullExecutionTime2, qwCont.fullExecutionTime3])
+    return timeList
 
 
 if __name__ == '__main__':
-    # Weird behavior on windows: scipy.linalg is 10x slower up until exactly 960.
-    n = 1000
+    n = 200
     print(n)
     t = int(n/2)
     gamma = 1/(2*np.sqrt(2))
     marked = [int(n/2)]
-    # graph = nx.cycle_graph(n)
-    graph = nx.complete_graph(n)
+    graph = nx.cycle_graph(n)
+    # graph = nx.complete_graph(n)
 
     samples = 100
-    eighTimeList = []
-    eighTimeList2 = []
-    eighTimeList3 = []
-
-    matMulTimeList = []
-    matMulTimeList2 = []
-    matMulTimeList3 = []
-
-    for i in range(samples):
-        print("\n--------- Trial #%s -------------------\n" % i)
-        qwCont = QuantumWalkDaoTest(n, graph , t, gamma, marked)
-        eighTimeList.append(qwCont.eighExecutionTime)
-        eighTimeList2.append(qwCont.eighExecutionTime2)
-        eighTimeList3.append(qwCont.eighExecutionTime3)
-        matMulTimeList.append(qwCont.matMulExecutionTime)
-        matMulTimeList2.append(qwCont.matMulExecutionTime2)
-        matMulTimeList3.append(qwCont.matMulExecutionTime3)
-
-    avgTimeEigh = getAvgTime(eighTimeList)
-    avgTimeEigh2 = getAvgTime(eighTimeList2)
-    avgTimeEigh3 = getAvgTime(eighTimeList3)
+    timeList = createTimeList(samples, n, graph, t, gamma, marked)
+    avgTimeList = getAvgTime(timeList)
 
     print("Avg eigh: %ss\nAvg eigh2: %ss\nAvg eigh3: %s\n\t for N = %s and %s samples." % (
-        avgTimeEigh, avgTimeEigh2, avgTimeEigh3, n, samples))
-    print(avgTimeEigh2)
-    print(avgTimeEigh3)
+        avgTimeList[0], avgTimeList[1], avgTimeList[2], n, samples))
     print("\n> Eigh3 is %s times faster than Eigh2&1" %
-          (avgTimeEigh/avgTimeEigh3))
-
-    avgTimeMatMult = getAvgTime(matMulTimeList)
-    avgTimeMatMult2 = getAvgTime(matMulTimeList2)
-    avgTimeMatMult3 = getAvgTime(matMulTimeList3)
-
+          (avgTimeList[0]/avgTimeList[2]))
     print("\nAvg MatMult: %ss\nAvg MatMult2: %ss\nAvg MatMult3: %s\n\t for N = %s and %s samples." % (
-        avgTimeMatMult, avgTimeMatMult2, avgTimeMatMult3, n, samples))
+        avgTimeList[3], avgTimeList[4], avgTimeList[5], n, samples))
+    print("\n> MatMult2 is %s times faster than MatMult\n" %
+          (avgTimeList[3]/avgTimeList[4]))
 
-    print("\n> MatMult2 is %s times faster than MatMult" %
-          (avgTimeMatMult/avgTimeMatMult2))
+    print("Avg full: %ss\nAvg full2: %ss\nAvg full3: %s\n\t for N = %s and %s samples." % (
+        avgTimeList[6], avgTimeList[7], avgTimeList[8], n, samples))
+    print("\n> Full2 is %s times faster than Full" %
+          (avgTimeList[6]/avgTimeList[7]))
+    print("\n> Full3 is %s times faster than Full2" %
+          (avgTimeList[7]/avgTimeList[8]))
 
-    # qwController = QuantumWalkDaoTest(n, nx.cycle_graph(n), t, gamma, marked)
-    # print("Version: np.Eig @ D @ np.EigH\n")
-    # print("\tFull run time: %ss" % round(qwController.fullExecutionTime, 5))
-    # print("\t\t--> Eigh time:   %ss" %
-    #       round(qwController.eighExecutionTime, 5))
-    # print("\t\t--> Diag time:   %ss" %
-    #       round(qwController.diagExecutionTime, 5))
-    # print("\t\t--> MatMul time: %ss\n" %
-    #       round(qwController.matMulExecutionTime, 2))
-    # print("Version: (np.Eig * D.toList) @ np.EigH\n")
-    # print("\tFull run time: %ss" % round(qwController.fullExecutionTime2, 5))
-    # print("\t\t--> Eigh time: %ss" % round(qwController.eighExecutionTime2, 5))
-    # print("\t\t--> Diag time: %ss" % round(qwController.diagExecutionTime2, 5))
-    # print("\t\t--> MatMul time: %ss\n" %
-    #       round(qwController.matMulExecutionTime2, 5))
-    # print("Version: (ln.Eig * D.toList) @ ln.EigH\n")
-    # print("\tFull run time: %ss" % round(qwController.fullExecutionTime3, 5))
-    # print("\t\t--> Eigh time: %ss" % round(qwController.eighExecutionTime3, 5))
-    # print("\t\t--> Diag time: %ss" % round(qwController.diagExecutionTime3, 5))
-    # print("\t\t--> MatMul time: %ss\n" %
-    #       round(qwController.matMulExecutionTime3, 5))
 
-    # k=1
-    # for eigh in [eighTimeList,eighTimeList2,eighTimeList3]:
-    #     plt.plot(eigh,label='%s'%k)
-    #     k+=1
-    #     plt.legend()
+        # k=1
+        # for eigh in [eighTimeList,eighTimeList2,eighTimeList3]:
+        #     plt.plot(eigh,label='%s'%k)
+        #     k+=1
+        #     plt.legend()
 
-    # k=1
-    # for eigh in [matMulTimeList,matMulTimeList2,matMulTimeList3]:
-    #     plt.plot(eigh,label='%s'%k)
-    #     k+=1
-    #     plt.legend()
+        # k=1
+        # for eigh in [matMulTimeList,matMulTimeList2,matMulTimeList3]:
+        #     plt.plot(eigh,label='%s'%k)
+        #     k+=1
+        #     plt.legend()
 
-    # ax = plt.gca()
-    # ax.set_ylim([-0.1, 0.4])
-    # plt.show()
+        # ax = plt.gca()
+        # ax.set_ylim([-0.1, 0.4])
+        # plt.show()
