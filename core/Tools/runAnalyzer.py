ec4da9458250eac0f03e1877ceb59387c6ed2ff5
Jaime Santos <50669048+JaimePSantos@users.noreply.github.com>
2022-03-31 21:59:23 +0100

    Updating profiling

--- core/Tools/runAnalyzer.py
+++ core/Tools/runAnalyzer.py
@@ -1,22 +1,50 @@
 import os
+from QuantumWalkTest.QwakBenchmarkStub import QWAKBenchmark
+import networkx as nx
+
 
 retval = os.getcwd()
 os.chdir(retval + "\\TestOutput\\Profiling\\")
 
+def runQwakProfiler(graph,t,marked,k0,k1):
+    qwController = QWAKBenchmark(graph, laplacian=False)
+    qwController.runWalk(t, marked)
+    qwController.getMean()
+    qwController.getSndMoment()
+    qwController.getStDev()
+    qwController.getSurvivalProb(k0, k1)
+    qwController.getInversePartRatio()
 
-def marshalFile(fileName, filePath):
+def unmarshallFile(fileName, filePath):
     os.chdir(filePath)
     timeDict = {}
+    multipleEntries = 0
     with open(fileName) as f:
         lines = f.readlines()
         for line in lines:
             if len(line) < 6 or "ncalls" in line:
                 continue
+            elif "Next Entry" in line:
+                multipleEntries += 1
+                continue
             else:
                 l = line.split(",")
-                timeDict[l[5]] = l[1]
+                if multipleEntries < 1:
+                    timeDict[l[5]] = float(l[1])
+                else:
+                    if l[5] not in timeDict:
+                        continue
+                    timeDict[l[5]] += float(l[1])
 
-    print(timeDict)
+    return timeDict
 
+n = 1000
+t = 10
+graph = nx.cycle_graph(n)
+marked = [20]
+k0 = 19
+k1 = 21
 
-marshalFile("__init__.prof", "qwak/")
+runQwakProfiler(graph,t,marked,k0,k1)
+timeDict = unmarshallFile("__init__.prof", "qwak/")
+print(timeDict)
