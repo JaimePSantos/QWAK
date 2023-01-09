04385a6094fb7dfda1147fc4607a1ca0aed48c5b
Jaime Santos <50669048+JaimePSantos@users.noreply.github.com>
2023-01-09 23:19:41 +0000

    Update for heroku deployment.

--- gui-flask/flaskqwak.py
+++ gui-flask/flaskqwak.py
@@ -20,8 +20,8 @@ QWAKCLUSTER_PASSWORD = os.environ.get('QWAKCLUSTER_PASSWORD')
 
 connection_string = f"mongodb+srv://{QWAKCLUSTER_USERNAME}:{QWAKCLUSTER_PASSWORD}@qwakcluster.kkszzg0.mongodb.net/test"
 
-client = MongoClient('localhost', 27017)
-# client = MongoClient(connection_string)
+# client = MongoClient('localhost', 27017)
+client = MongoClient(connection_string)
 db_string = 'qwak_flask'
 database = client.get_database(db_string)
 probDistEntry = database['probDistEntry']
