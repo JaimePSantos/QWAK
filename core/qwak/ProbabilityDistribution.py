2a53f0636e904d8a759360c7c061f4bba277dab5
Jaime Santos <50669048+JaimePSantos@users.noreply.github.com>
2023-01-12 17:40:02 +0000

    ProbDist documentation. Cleaning up some functions.

--- core/qwak/ProbabilityDistribution.py
+++ core/qwak/ProbabilityDistribution.py
@@ -1,13 +1,12 @@
-import warnings
+from __future__ import annotations
+from typing import Union
 
 import numpy as np
 from qwak.State import State
 from qwak.Errors import MissingNodeInput
 import json
 from utils.jsonMethods import json_matrix_to_complex, complex_matrix_to_json
-
-warnings.filterwarnings("ignore")
-
+from functools import reduce
 
 class ProbabilityDistribution:
     """
@@ -32,7 +31,15 @@ class ProbabilityDistribution:
         self._n = state.getDim()
         self._probVec = np.zeros(self._n, dtype=float)
 
-    def to_json(self):
+    def to_json(self) -> str:
+        """
+            Converts the ProbabilityDistribution object to a JSON string.
+
+        Returns
+        -------
+        str
+            JSON string of the ProbabilityDistribution object.
+        """   
         return json.dumps({
             'state': json.loads(self._state.to_json()),
             'dim': self._n,
@@ -40,7 +47,19 @@ class ProbabilityDistribution:
         })
 
     @classmethod
-    def from_json(cls, json_var):
+    def from_json(cls, json_var: Union[str, dict]) -> ProbabilityDistribution:
+        """Converts a JSON string to a ProbabilityDistribution object. 
+
+        Parameters
+        ----------
+        json_var : Union[str, dict]
+            JSON string or dictionary to be converted to a ProbabilityDistribution object.
+
+        Returns
+        -------
+        ProbabilityDistribution
+            ProbabilityDistribution object converted from JSON.
+        """        
         if isinstance(json_var, str):
             json_dict = json.loads(json_var)
         elif isinstance(json_var, dict):
@@ -64,7 +83,7 @@ class ProbabilityDistribution:
         Parameters
         ----------
         state : State, optional
-            _description_, by default None
+            State to be converted into a probability, by default None
         """
         if state is not None:
             self._n = state.getDim()
@@ -73,13 +92,13 @@ class ProbabilityDistribution:
         self._probVec = np.array(
             [((state.item(0) * np.conj(state.item(0))).real) for state in self._stateVec])
 
-    def setProbDist(self, newProbDist) -> None:
-        """_summary_
+    def setProbDist(self, newProbDist: ProbabilityDistribution) -> None:
+        """Sets the current probability distribution to a user inputted one.
 
         Parameters
         ----------
-        newProbDist : _type_
-            _description_
+        newProbDist : ProbabilityDistribution
+            New probability distribution for the object.
         """
         self._n = newProbDist.getDim()
         self._state.setState(newProbDist.getState())
@@ -87,32 +106,53 @@ class ProbabilityDistribution:
         self._probVec = newProbDist.getProbVec()
 
     def getStateVec(self) -> State:
-        """_summary_
+        """Gets the state vector associated with a distribution.
 
         Returns
         -------
         State
-            _description_
+            Returns the state vector of the ProbabilityDistribution object.
         """
         return self._stateVec
 
-    def getState(self):
+    def getState(self) -> State:
+        """Gets the state associated with a distribution.
+
+        Returns
+        -------
+        State
+            Returns the state of the ProbabilityDistribution object.
+        """        
         return self._state
 
-    def setState(self, newState):
+    def setState(self, newState: State) -> None:
+        """Sets the current state to a user inputted one.
+
+        Parameters
+        ----------
+        newState : State
+            New state for the distribution.
+        """        
         self._state.setState(newState)
 
-    def setDim(self, newDim):
+    def setDim(self, newDim: int) -> None:
+        """Sets the current dimension to a user inputted one.
+
+        Parameters
+        ----------
+        newDim : int
+            New dimension for the distribution.
+        """        
         self._n = newDim
         self._probVec = np.zeros(self._n, dtype=float)
 
     def getDim(self) -> int:
-        """_summary_
+        """Gets the dimension associated with a distribution.
 
         Returns
         -------
         int
-            _description_
+            Returns the dimension of the ProbabilityDistribution object.
         """
         return self._n
 
@@ -151,33 +191,18 @@ class ProbabilityDistribution:
         """
         return self._probVec.item(searchNode)
 
-    def mean(self) -> float:
-        """Gets the mean of the current probability distribution.
-
-        Returns
-        -------
-        float
-            Mean of the ProbabilityDistribution object.
-        """
-        # TODO: This function has been replaced by moment(1).
-        pos = np.arange(0, self._n)
-        m = 0
-        for x in range(self._n):
-            m += pos[x] * self._probVec[x]
-        return float(m)
-
-    def moment(self, k) -> float:
-        """_summary_
+    def moment(self, k:int) -> float:
+        """Calculates the kth moment of the probability distribution.
 
         Parameters
         ----------
-        k : _type_
-            _description_
-
+        k : int
+            User inputted moment.
+            
         Returns
         -------
         float
-            _description_
+            kth moment of the probability distribution.
         """
         pos = np.arange(0, self._n)
         m = 0
@@ -185,52 +210,53 @@ class ProbabilityDistribution:
             m += (pos[x] ** k) * self._probVec[x]
         return float(m)
 
-    def invPartRatio(self):
-        invPartRatio = 0
-        for prob in self._probVec:
-            invPartRatio += np.absolute(prob.item(0))**2
-        invPartRatio = 1 / invPartRatio
-        return invPartRatio
-
-    def stDev(self) -> float:
-        """_summary_
+    def invPartRatio(self)-> float:
+        """Calculates the inverse participation ratio of the probability distribution.
 
         Returns
         -------
         float
-            _description_
+            Inverse participation ratio of the probability distribution.
+        """
+        return 1/(np.sum(np.absolute(self._probVec)**2))
+
+    def stDev(self) -> float:
+        """Calculates the standard deviation of the probability distribution.
+
+        Returns
+        -------
+        float
+            Standard deviation of the probability distribution.
         """
         stDev = self.moment(2) - self.moment(1) ** 2
-        if stDev <= 0:
-            return 0
-        return np.sqrt(stDev)
+        return np.sqrt(stDev) if (stDev>0) else 0
 
-    def survivalProb(self, k0, k1) -> float:
-        """_summary_
+    def survivalProb(self, fromNode, toNode) -> float:
+        """Calculates the survival probability of the probability distribution.
 
         Parameters
         ----------
-        k0 : _type_
-            _description_
-        k1 : _type_
-            _description_
+        fromNode : _type_
+            Starting Node.
+        toNode : _type_
+            Ending node.
 
         Returns
         -------
         float
-            _description_
+            Survival probability of the probability distribution.
         """
         survProb = 0
         try:
-            if k0 == k1:
+            if fromNode == toNode:
                 return self._probVec[int(k0)][0]
             else:
-                for i in range(int(k0), int(k1) + 1):
+                for i in range(int(fromNode), int(toNode) + 1):
                     survProb += self._probVec[i]
             return survProb
         except ValueError:
             raise MissingNodeInput(
-                f"A node number is missing: k0 = {k0}; k1={k1}")
+                f"A node number is missing: fromNode = {fromNode}; toNode={toNode}")
 
     def __str__(self) -> str:
         """String representation of the ProbabilityDistribution object.
