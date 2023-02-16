9044a3bd87e36d55db25a14b2edfc6f1851e8b6a
Jaime Santos <50669048+JaimePSantos@users.noreply.github.com>
2023-02-16 01:16:03 +0000

    searching seems to be working

--- core/utils/plotTools.py
+++ core/utils/plotTools.py
@@ -24,3 +24,25 @@ def searchProbStepsPlotting(qwak: QWAK):
         markedProbDistList.append(markedProbability)
         markedProbability = 0
     return markedProbDistList
+
+def searchProbStepsPlotting2(qwak: QWAK,probDistList):
+    """Plots the probability of finding the target as a function of the number of steps.
+
+    Parameters
+    ----------
+    qwak : QWAK
+        QWAK object containing the results of the simulation.
+    """
+    markedProbability = 0
+    markedProbDistList = []
+    markdElements = qwak.getMarkedElements()
+    probDistList  = probDistList
+    if probDistList == []:
+        raise EmptyProbDistList("The probability distribution list is empty.")
+    for probDist in probDistList:
+        for element in markdElements:
+            markedProbability += probDist.searchNodeProbability(element[0])
+        markedProbDistList.append(markedProbability)
+        markedProbability = 0
+    # print(markedProbDistList)
+    return markedProbDistList
