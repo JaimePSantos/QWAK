2d0b8ef3b3eb7d9b497100e95aebd82a5678946b
Jaime Santos <50669048+JaimePSantos@users.noreply.github.com>
2025-01-15 14:37:25 +0000

    benchmarking

--- benchmark/CuPy/qwak_cupy_benchmark_cycle.py
+++ benchmark/CuPy/qwak_cupy_benchmark_cycle.py
@@ -65,6 +65,24 @@ def runMultipleSimpleQWAK_cupy(nList,t,samples):
         qwak_time_average = 0
     return tList, qwList
 
+def runMultipleSimpleQWAK_cupy2(nList,t,samples):
+    #qwList = []
+    tList = []
+    qwak_time = 0
+    qwak_time_average = 0
+    print(f"Running CuPy QWAK {len(nList)} walks up to n = {nList[-1]}")
+    for n in nList:
+        # print(f"----> Calculating NP QWAK for n = {n}",end='\r')
+        for sample in range(1,samples+1):
+            print(f"----> Calculating CuPy QWAK for n = {n} \t Sample #{sample}",end='\r')
+            qw, qwak_time = runTimedQWAK_cupy(n,t)
+            qwak_time_average += qwak_time 
+        qwak_time_average = qwak_time_average / samples
+        # qwList.append(qw)
+        tList.append(qwak_time_average)
+        qwak_time_average = 0
+    return tList#, qwList
+
 def load_list_from_file(file_path):
     with open(file_path, 'r') as file:
         data_str = file.read()
@@ -76,23 +94,30 @@ def write_list_to_file(file_path, data):
     with open(file_path, 'w') as file:
         file.write('\n'.join(data_str))
 t = 50
-nMax = 500
-nList = list(range(3,nMax,5))
-samples = 1
+nMin = 800
+nMax = 1000
+nList = list(range(nMin,nMax,1))
+samples = 100
 
-qwak_times_file = f'Datasets/Benchmark-SimpleQWAK_Cycle/simpleQWAKTime_N{nList[0]}-{nList[-1]}_T{t}_S{samples}.txt'
-qwak_times_file_cupy = f'Datasets/Benchmark-SimpleQWAK_Cycle/simpleQWAKTime_CuPy_N{nList[0]}-{nList[-1]}_T{t}_S{samples}.txt'
+qwak_times_file = f'Datasets/Benchmark-SimpleQWAK_Cycle/simpleQWAKTime_N{nMin}-{nMax-1}_T{t}_S{samples}.txt'
+qwak_times_file_cupy = f'Datasets/Benchmark-SimpleQWAK_Cycle/simpleQWAKTime_CuPy_N{nMin}-{nMax-1}_T{t}_S{samples}.txt'
 
-if os.path.exists(qwak_times_file):
-    qwak_times = load_list_from_file(qwak_times_file)
-    print('File Exists!')
-else:
-    qwak_times,qw = runMultipleSimpleQWAK(nList,t,samples)
-    write_list_to_file(qwak_times_file,qwak_times)
+# if os.path.exists(qwak_times_file):
+#     qwak_times = load_list_from_file(qwak_times_file)
+#     print('File Exists!')
+# else:
+#     qwak_times,qw = runMultipleSimpleQWAK(nList,t,samples)
+#     write_list_to_file(qwak_times_file,qwak_times)
 
 if os.path.exists(qwak_times_file_cupy):
-    qwak_times_cupy = load_list_from_file(qwak_times_file)
+    qwak_times_cupy = load_list_from_file(qwak_times_file_cupy)
     print('File Exists!')
 else:
-    qwak_times_cupy ,qw = runMultipleSimpleQWAK_cupy(nList,t,samples)
+    qwak_times_cupy ,qw = runMultipleSimpleQWAK_cupy2(nList,t,samples)
     write_list_to_file(qwak_times_file_cupy,qwak_times)
+
+
+plt.plot(nList,qwak_times,label='QWAK CPU_NumPy')
+plt.plot(nList,qwak_times_cupy,label='QWAK GPU_CuPy')
+plt.legend()
+plt.show()
