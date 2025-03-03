c3f9c8622b5261cfd82bf218709657dece8b5f4e
Jaime Santos <50669048+JaimePSantos@users.noreply.github.com>
2025-03-03 02:22:57 +0000

    update

--- benchmark/Operator/Operator-ER-DynamicBenchmark.py
+++ benchmark/Operator/Operator-ER-DynamicBenchmark.py
@@ -35,60 +35,62 @@ def benchmark_operations(n, tList, base_filename, samples,pVal=0.8,seed=10):
         return eig_runTime, expm_runTime
     
     else:
-        print('Files not found!')
-        op = OperatorBenchmark3(graph)
-        opAdjTime = op.buildAdjacencyTimed()
-        opEigTime = op.buildEigenTimed()
-        op_init_run_time = opAdjTime + opEigTime
-
-        for time in tList:
-            total_runOp_run_time = 0
-            for _ in range(samples):
-                for t in range(0, time):
-                    runOp_run_time = op.buildDiagonalOperatorNoEigTimed(graph=graph, time=t)
-                    total_runOp_run_time += runOp_run_time
-            avg_runOp_run_time = total_runOp_run_time / samples
-            eig_runTime.append(avg_runOp_run_time + op_init_run_time)
-            op_init_run_time = 0
-
-        # Time the buildDiagonalOperator method for expm
-        op2 = OperatorBenchmark3(graph)
-        op2AdjTime = op2.buildAdjacencyTimed()
-
-        for time in tList:
-            print(f'Expm time: {time}')
-            total_runOp2_run_time = 0
-            for s in range(samples):
-                if s%20 == 0:
-                    print(f'\t----> Sample: {s}')
-                for t in range(0, time):
-                    runOp2_run_time = op2.buildSlowDiagonalOperatorNoAdjTimed(graph=graph, time=t)
-                    total_runOp2_run_time += runOp2_run_time
-            avg_runOp2_run_time = total_runOp2_run_time / samples
-            expm_runTime.append(avg_runOp2_run_time + op2AdjTime)
-            op2AdjTime = 0
-            
-        with open(eig_filename, "w", newline="") as f:
-            writer = csv.writer(f)
-            writer.writerow(["Eig RunTime"])
-            writer.writerow(eig_runTime)
-
-        # Save to CSV
-        with open(expm_filename, "w", newline="") as f:
-            writer = csv.writer(f)
-            writer.writerow(["Expm RunTime"])
-            writer.writerow(expm_runTime)
-
-        return eig_runTime, expm_runTime
-    
+         print('Files not found!')
+         op = OperatorBenchmark3(graph)
+         opAdjTime = op.buildAdjacencyTimed()
+         opEigTime = op.buildEigenTimed()
+         op_init_run_time = opAdjTime + opEigTime
+     
+         for time in tList:
+             total_runOp_run_time = 0
+             for _ in range(samples):
+                 for t in range(0, time):
+                     runOp_run_time = op.buildDiagonalOperatorNoEigTimed(graph=graph, time=t)
+                     total_runOp_run_time += runOp_run_time
+             avg_runOp_run_time = total_runOp_run_time / samples
+             eig_runTime.append(avg_runOp_run_time + op_init_run_time)
+             op_init_run_time = 0
+     
+         # Time the buildDiagonalOperator method for expm
+         op2 = OperatorBenchmark3(graph)
+         op2AdjTime = op2.buildAdjacencyTimed()
+     
+         for time in tList:
+             print(f'Expm time: {time}')
+             total_runOp2_run_time = 0
+             for s in range(samples):
+                 if s % 20 == 0:
+                     print(f'\t----> Sample: {s}')
+                 for t in range(0, time):
+                     runOp2_run_time = op2.buildSlowDiagonalOperatorNoAdjTimed(graph=graph, time=t)
+                     total_runOp2_run_time += runOp2_run_time
+             avg_runOp2_run_time = total_runOp2_run_time / samples
+             expm_runTime.append(avg_runOp2_run_time + op2AdjTime)
+             op2AdjTime = 0
+     
+         # Create the directory if it doesn't exist
+         directory = os.path.dirname(eig_filename)
+         os.makedirs(directory, exist_ok=True)
+     
+         with open(eig_filename, "w", newline="") as f:
+             writer = csv.writer(f)
+             writer.writerow(["Eig RunTime"])
+             writer.writerow(eig_runTime)
+     
+         with open(expm_filename, "w", newline="") as f:
+             writer = csv.writer(f)
+             writer.writerow(["Expm RunTime"])
+             writer.writerow(expm_runTime)
+     
+         return eig_runTime, expm_runTime
 
 
 # Example usage
-n = 30
+n = 100
 start = 1
-stop = n
+stop = 30
 step = 1
-samples = 200
+samples = 100
 legend_labels = ['Diagonal', 'Expm']
 
 params = {
@@ -103,7 +105,7 @@ params = {
     # 'color_list' : ['#0000FF', '#008000', '#525252'],
     'color_list' : ['b','g','r'],
     # 'line_style_list' : ['--', '-','-.' ],
-    'save_path' : f'img/ER/dynamicBenchmark_WSTART{start}_WEND{stop}_WST{step}_SAMP{samples}.png',
+    # 'save_path' : f'img/ER/dynamicBenchmark_WSTART{start}_WEND{stop}_WST{step}_SAMP{samples}.png',
     'use_loglog': False,
     'use_cbar' : False,
     'cbar_label' : None, 
