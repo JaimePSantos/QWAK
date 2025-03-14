6ffea33b62f21dd6cef143bb0aee0cd064491b85
Jaime Santos <50669048+JaimePSantos@users.noreply.github.com>
2025-03-14 22:55:21 +0000

    update

--- tests/test_StochasticQwak.py
+++ tests/test_StochasticQwak.py
@@ -2,9 +2,9 @@ import networkx as nx
 import numpy as np
 
 from qwak.StochasticQwak import StochasticQWAK
-from stubs.StochasticQWAKTestStub import StochasticQWAKTestStub
+from tests.stubs.StochasticQWAKTestStub import StochasticQWAKTestStub
 
-from testVariables.stochasticQwakVar import (
+from tests.testVariables.stochasticQwakVar import (
     stochasticProbDistSingleNodeCycleNoise,
     stochasticProbDistSingleNodeCycleNoNoise,
 )
@@ -20,14 +20,14 @@ class TestStochasticQWAK(object):
             np.zeros(n),
             err_msg="Probability distribution before running should be 0.",
         )
-        print("Before calling buildWalk")
         qwak.buildWalk(t)
-        print("After calling buildWalk")
+        # print("Before calling runWalk")
+        # print("After calling runWalk")
 
 
     def test_StochasticProbDistSingleNodeCycleNoise(self):
         n = 50
-        t = 1
+        t = 20
         self.t = t
         noiseParam = 0.15
         sinkNode = 10
@@ -55,7 +55,3 @@ class TestStochasticQWAK(object):
             stochasticProbDistSingleNodeCycleNoise,
             err_msg=f"Probability Distribution does not match expected for n = {n} and t = {t}",
         )
-
-bench = TestStochasticQWAK()
-bench.test_StochasticProbDistSingleNodeCycleNoNoise()
-# bench.test_StochasticProbDistSingleNodeCycleNoise()
